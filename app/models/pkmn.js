10cbd10757bc7c396f26d9cf8983dc081f6423ad
Rosanne94 <39834489+Rosanne94@users.noreply.github.com>
2018-06-06 17:25:31 +0200

    Add files via upload

--- app/models/pkmn.js
+++ app/models/pkmn.js
@@ -2,9 +2,17 @@ export const pkmns = {
   state:{
     pkmns: [],
     //id, naam, type, img, flavourtext, mogelijke evoluties
-  }
+  },
 
   reducers: {
     add: (state, payload) => state + payload,
+  },
+
+  effects:{
+    async loadData(payload, rootState) {
+      const response = await fetch('http://pokeapi.co/api/v2/pokemon-species/?limit=3')
+      const data = await response.json()
+      dispatch.add.update(data)
+    }
   }
-}
+};
